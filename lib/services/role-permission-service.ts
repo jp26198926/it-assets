60763d0115d336a0562eb63dea8b394cc86c4f4a
Jaypee <eujay.29@gmail.com>
2026-06-21 18:37:23 +1000

    fix: invalidate authorization cache when role permissions are added or removed

--- lib/services/role-permission-service.ts
+++ lib/services/role-permission-service.ts
@@ -1,4 +1,5 @@
 import * as roleService from "@/lib/services/role-service";
+import { invalidateCache } from "@/lib/services/authorization-service";
 import type { CreateRolePermissionInput, RolePermissionEntry } from "@/lib/types/role-permission";
 
 export async function getRolePermissions(roleId: string): Promise<RolePermissionEntry[]> {
@@ -6,9 +7,11 @@ export async function getRolePermissions(roleId: string): Promise<RolePermission
 }
 
 export async function createRolePermission(roleId: string, data: CreateRolePermissionInput): Promise<void> {
-  return roleService.addRolePermission(roleId, data.page_id, data.permission_id);
+  await roleService.addRolePermission(roleId, data.page_id, data.permission_id);
+  invalidateCache(roleId);
 }
 
 export async function deleteRolePermission(roleId: string, pageId: string, permissionId: string): Promise<void> {
-  return roleService.removeRolePermission(roleId, pageId, permissionId);
+  await roleService.removeRolePermission(roleId, pageId, permissionId);
+  invalidateCache(roleId);
 }
